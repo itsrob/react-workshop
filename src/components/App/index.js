e1afc3843d67550538df26934548ddb6330a53f8
Philipps <mail@robert-philipps.com>
2018-08-30 14:17:41 +0200

    add reduced menu for smaller width

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,9 +2,25 @@ import React from 'react'
 import Navigation from '../Navigation'
 import Router from '../Router'
 
+const pages = [
+  {title: 'Hello World', url: '/HelloWorld'},
+  {title: 'Simple Props', url: '/SimpleProps'},
+  {title: 'Complete props', url: '/CompleteProps'},
+  {title: 'React Children', url: '/Children'},
+  {title: 'Events', url: '/Events'},
+  {title: 'Lifecycle', url: '/Lifecycle'},
+  {title: 'Stateful components', url: '/State'},
+  {title: 'State management', url: '/StateManagement'},
+  {title: 'Higher-order functions', url: '/HigherOrder'},
+  {title: 'Styling', url: '/Styling'},
+  {title: 'Your Move', url: '/YourMove'},
+  {title: 'What’s next?', url: '/Next'},
+  {title: 'About us', url: '/About'}
+]
+
 const App = () => (
   <div>
-    <Navigation />
+    <Navigation pages={pages}/>
     <Router />
   </div>
 )
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -7,76 +7,24 @@ const Navigation = props => (
   <nav className="Navigation">
     <Logo className="Logo" />
     <ol className="NavigationList">
-      <li className="NavigationItem">
+      <div>
         <NavLink exact to="/" className="NavigationLink">
-          Home
+          <li className="NavigationItem">
+            <span>Home</span>
+          </li>
         </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/HelloWorld" className="NavigationLink">
-          Hello World
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/SimpleProps" className="NavigationLink">
-          Simple Props
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/CompleteProps" className="NavigationLink">
-          Complete props
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/Children" className="NavigationLink">
-          React Children
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/Events" className="NavigationLink">
-          Events
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/Lifecycle" className="NavigationLink">
-          Lifecycle
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/State" className="NavigationLink">
-          Stateful components
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/StateManagement" className="NavigationLink">
-          State management
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/HigherOrder" className="NavigationLink">
-          Higher-order functions
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/Styling" className="NavigationLink">
-          Styling
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/YourMove" className="NavigationLink">
-          Your Move
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/Next" className="NavigationLink">
-          What’s next?
-        </NavLink>
-      </li>
-      <li className="NavigationItem">
-        <NavLink to="/About" className="NavigationLink">
-          About us
-        </NavLink>
-      </li>
+      </div>
+
+      {props.pages.map(page => (
+        <div key={page.title}>
+          <NavLink to={page.url} className="NavigationLink">
+            <li className="NavigationItem">
+              <span>{page.title}</span>
+            </li>
+          </NavLink>
+        </div>
+    ))}
+
     </ol>
   </nav>
 )
